872e13f32e3baedddb50b4a5b9e9a8837960d770
sheyesi <2461673502@qq.com>
2025-10-08 16:36:26 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// import { add } from './tools/index'
+
+// console.log(add(1,2)); 
+
+import './style/style.css'; 
+const el = document.createElement('div')
+el.className = 'title'
+el.innerHTML = "这是小庄zzz的标题"
+document.body.appendChild(el)
